d61dc1bb3c2025bbfce6b07e6268ac7c8c760140
Carlos Astori <tadeu.astori@gmail.com>
2024-08-15 11:36:00 -0400

    added Angular material design

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,12 +11,13 @@ import { dbConfig } from './database/database.config';
 import { provideStore } from '@ngxs/store';
 import { ApplicationState } from './core/states/application.state';
 import { HammerModule } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     importProvidersFrom([NgxIndexedDBModule.forRoot(dbConfig), HammerModule]),
-    provideStore([ApplicationState]),
+    provideStore([ApplicationState]), provideAnimationsAsync(),
   ],
 };
