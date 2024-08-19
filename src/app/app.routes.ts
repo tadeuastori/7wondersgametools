04dbd302e25168d495b77a179c7736b97db04a94
Carlos Astori <tadeu.astori@gmail.com>
2024-08-19 12:52:52 -0400

    generate page path in the menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { ScoreSheetComponent } from './features/score-sheet/score-sheet.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full', title: 'Home' },
   { path: '**', redirectTo: 'page-not-found' },
 
   // TODO: add routes
   {
     path: 'score-sheet-menu',
     component: ScoreSheetComponent,
+    title: 'Score Sheet',
     // children: [{ path: '', redirectTo: 'home', pathMatch: 'full' }],
   },
 ];
