19df5e837d6b5a5ef784d9f472a0345e531b2531
Carlos Astori <tadeu.astori@gmail.com>
2025-10-07 21:16:53 -0400

    adding player list from store

--- src/app/shared/components/dialog/add-player/add-player.component.ts
+++ src/app/shared/components/dialog/add-player/add-player.component.ts
@@ -9,10 +9,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { IMatchPlayers, MatchPlayers } from '@score-sheet-menu/models/match-players.model';
+import { Store } from '@ngxs/store';
+// import { IMatchPlayers, MatchPlayers } from '@score-sheet-menu/models/match-players.model';
 import { Observable, startWith, map } from 'rxjs';
 import { EWonderSide } from 'src/app/core/enums/wonder-side.enum';
 import { IPlayer, Player } from 'src/app/core/models/player/player.model';
+import { ApplicationStateSelectors } from 'src/app/core/states/application.queries';
 
 @Component({
   selector: 'app-add-player',
@@ -39,6 +41,8 @@ export class AddPlayerComponent implements OnInit, AfterViewInit {
     | ElementRef
     | undefined;
 
+  _store = inject(Store);
+
   readonly dialogRef = inject(MatDialogRef<AddPlayerComponent>);
   readonly data = inject<any>(MAT_DIALOG_DATA);
   readonly wonderList: ModelSignal<Array<{ name: string; icon?: string }>> = model(this.data.wonders);
@@ -47,10 +51,17 @@ export class AddPlayerComponent implements OnInit, AfterViewInit {
   readonly hasWonderSide: ModelSignal<boolean> = model(this.data.hasWonderSide);
 
   autoCompleteOptions: Observable<string[]> = new Observable<string[]>();
+  applicationPlayersList$: Observable<IPlayer[]>;
 
   selectedWonder: string[] = [];
   newPlayerControl = new FormControl<string>('');
   wonderSide: boolean = !this.hasWonderSide();
+
+    constructor() {
+    this.applicationPlayersList$ = this._store.select(
+      ApplicationStateSelectors.getApplicationPlayers
+    );
+  }
 
   ngOnInit() {
     this.autoCompleteOptions = this.newPlayerControl.valueChanges.pipe(
@@ -135,6 +146,32 @@ export class AddPlayerComponent implements OnInit, AfterViewInit {
 
     this.dialogRef.close(newPlayer);
   }
+
+    private _loadPlayerList(): void {
+      this.originalPlayersList = [];
+  
+      this.applicationPlayersList$
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((players: IPlayer[]) => {
+          players
+            .filter((player: IPlayer) => player.name)
+            .forEach((player: IPlayer) => {
+              if (
+                !this.originalPlayersList.map((p) => p.name).includes(player.name)
+              ) {
+                this.originalPlayersList.push({
+                  id: player.id,
+                  name: player.name,
+                });
+              }
+            });
+        });
+  
+      this.originalPlayersList = SortUtils.sortByProperty(
+        this.originalPlayersList,
+        'name'
+      );
+    }
 
   sun: string =
     'M12 15.5q1.45 0 2.475-1.025Q15.5 13.45 15.5 12q0-1.45-1.025-2.475Q13.45 8.5 12 8.5q-1.45 0-2.475 1.025Q8.5 10.55 8.5 12q0 1.45 1.025 2.475Q10.55 15.5 12 15.5Zm0 1.5q-2.075 0-3.537-1.463T7 12q0-2.075 1.463-3.537T12 7q2.075 0 3.537 1.463T17 12q0 2.075-1.463 3.537T12 17ZM1.75 12.75q-.325 0-.538-.213Q1 12.325 1 12q0-.325.212-.537Q1.425 11.25 1.75 11.25h2.5q.325 0 .537.213Q5 11.675 5 12q0 .325-.213.537-.213.213-.537.213Zm18 0q-.325 0-.538-.213Q19 12.325 19 12q0-.325.212-.537.212-.213.538-.213h2.5q.325 0 .538.213Q23 11.675 23 12q0 .325-.212.537-.212.213-.538.213ZM12 5q-.325 0-.537-.213Q11.25 4.575 11.25 4.25v-2.5q0-.325.213-.538Q11.675 1 12 1q.325 0 .537.212 .213.212 .213.538v2.5q0 .325-.213.537Q12.325 5 12 5Zm0 18q-.325 0-.537-.212-.213-.212-.213-.538v-2.5q0-.325.213-.538Q11.675 19 12 19q.325 0 .537.212 .213.212 .213.538v2.5q0 .325-.213.538Q12.325 23 12 23ZM6 7.05l-1.425-1.4q-.225-.225-.213-.537.013-.312.213-.537.225-.225.537-.225t.537.225L7.05 6q.2.225 .2.525 0 .3-.2.5-.2.225-.513.225-.312 0-.537-.2Zm12.35 12.375L16.95 18q-.2-.225-.2-.538t.225-.512q.2-.225.5-.225t.525.225l1.425 1.4q.225.225 .212.538-.012.313-.212.538-.225.225-.538.225t-.538-.225ZM16.95 7.05q-.225-.225-.225-.525 0-.3.225-.525l1.4-1.425q.225-.225.538-.213.313 .013.538 .213.225 .225.225 .537t-.225.537L18 7.05q-.2.2-.512.2-.312 0-.538-.2ZM4.575 19.425q-.225-.225-.225-.538t.225-.538L6 16.95q.225-.225.525-.225.3 0 .525.225 .225.225 .225.525 0 .3-.225.525l-1.4 1.425q-.225.225-.537.212-.312-.012-.537-.212ZM12 12Z';
