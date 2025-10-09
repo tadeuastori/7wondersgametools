d93467d6cb730b3ffbf3b9679e2f5b9ddf11103b
Carlos Astori <tadeu.astori@gmail.com>
2025-10-08 22:28:36 -0400

    fix issue with provide

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,9 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { dbConfig } from './database/database.config';
+import { provideIndexedDb } from './database/database.config';
 import { provideStore } from '@ngxs/store';
 import { ApplicationState } from './core/states/application.state';
-import { provideIndexedDb } from 'ngx-indexed-db';
 import { provideHttpClient } from '@angular/common/http';
 import { TranslocoHttpLoader } from './core/services/transloco/transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
@@ -18,7 +17,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore([ApplicationState]),
-    provideIndexedDb(dbConfig), provideHttpClient(), provideTransloco({
+    provideIndexedDb(), 
+    provideHttpClient(), 
+    provideTransloco({
         config: { 
           availableLangs: ['pt-br','en'],
           defaultLang: 'pt-br',
