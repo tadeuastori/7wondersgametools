cd351148bb3a6d5bf9e6c09e7397b35916c781b7
Carlos Astori <tadeu.astori@gmail.com>
2025-10-08 21:42:28 -0400

    create database schema

--- src/app/database/database.config.ts
+++ src/app/database/database.config.ts
@@ -1,9 +1,9 @@
 import { DBConfig } from 'ngx-indexed-db';
 import { playerSchema } from './schemas/player.schema';
-import { matchSchema } from './schemas/match.schema';
+import { settingsSchema } from './schemas/settings.schema';
 
 export const dbConfig: DBConfig = {
   name: '7WondersGameTools',
   version: 1,
-  objectStoresMeta: [playerSchema],
+  objectStoresMeta: [settingsSchema, playerSchema],
 };
