916b69f05aace2373d877af7af18dfa1da566daa
Carlos Astori <tadeu.astori@gmail.com>
2025-10-10 18:00:50 -0400

    created wonders compoenet

---
+++ src/app/features/setting/pages/wonders/wonders.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wonders',
+  imports: [],
+  templateUrl: './wonders.component.html',
+  styleUrl: './wonders.component.less'
+})
+export class WondersComponent {
+
+}
