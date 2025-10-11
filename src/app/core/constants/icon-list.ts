80f986d98c0f05b2f2d71f4ad89c31be33d60135
Carlos Astori <tadeu.astori@gmail.com>
2025-10-11 01:38:50 -0400

    added icon list and model

---
+++ src/app/core/constants/icon-list.ts
@@ -0,0 +1,7 @@
+import { IIcon } from "../models/setting/icon.model";
+
+export const iconsList: IIcon[] = [
+    //flags
+      { name: 'en', path: '/images/flags/en-flag.svg' },
+      { name: 'pt-br', path: '/images/flags/br-flag.svg' },
+    ];
