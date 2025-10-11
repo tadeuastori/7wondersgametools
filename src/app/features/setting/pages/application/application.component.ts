8d6f1e3f2da7294ee0204510cd0eda3a999dcdb1
Carlos Astori <tadeu.astori@gmail.com>
2025-10-10 22:04:29 -0400

    created application controller

---
+++ src/app/features/setting/pages/application/application.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application',
+  imports: [],
+  templateUrl: './application.component.html',
+  styleUrl: './application.component.less'
+})
+export class ApplicationComponent {
+
+}
