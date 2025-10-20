5b7732f2edf0f15d0336e106bc97c36d662c5295
Carlos Astori <tadeu.astori@gmail.com>
2025-10-20 13:59:21 -0400

    deploy

--- src/app/core/constants/icon-flag-list.constant.ts
+++ src/app/core/constants/icon-flag-list.constant.ts
@@ -2,7 +2,7 @@ import { IIcon } from "../models/setting/icon.model";
 
 export const iconsList: IIcon[] = [
     //icon flags to register
-      { name: 'os', path: '/assets/images/flags/os-flag.svg' },
-      { name: 'en-ca', path: '/assets/images/flags/en-ca-flag.svg' },
-      { name: 'pt-br', path: '/assets/images/flags/br-flag.svg' },
+      { name: 'os', path: 'assets/images/flags/os-flag.svg' },
+      { name: 'en-ca', path: 'assets/images/flags/en-ca-flag.svg' },
+      { name: 'pt-br', path: 'assets/images/flags/br-flag.svg' },
     ];
