3a420427f0f5368e872fca9ecc8c4ebb64407df2
Carlos Astori <tadeu.astori@gmail.com>
2025-10-21 10:08:07 -0400

    created db schema

--- src/app/database/schemas/match.schema.ts
+++ src/app/database/schemas/match.schema.ts
@@ -1,11 +1,11 @@
 import { ObjectStoreMeta } from 'ngx-indexed-db';
 
 export const matchSchema: ObjectStoreMeta = {
-  store: 'match',
+  store: 'matchGame',
   storeConfig: { keyPath: 'id', autoIncrement: true },
   storeSchema: [
     { name: 'matchDate', keypath: 'matchDate', options: { unique: false } },
-    { name: 'game', keypath: 'game', options: { unique: false } },
+    { name: 'gameType', keypath: 'gameType', options: { unique: false } },
     { name: 'expansions', keypath: 'expansions', options: { unique: false } },
     { name: 'players', keypath: 'players', options: { unique: false } },
   ],
