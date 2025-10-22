ca24d850c0050a919675f9d06e9f42198f63c37f
Carlos Astori <tadeu.astori@gmail.com>
2025-10-22 16:21:21 -0400

    organize stages

--- src/app/core/enums/stages.enum.ts
+++ src/app/core/enums/stages.enum.ts
@@ -1,4 +1,5 @@
 export enum EStages {
+  //base stages
   WONDER = 'Wonder',
   COIN = 'Coin',
   DEBT = 'Debt',
@@ -8,13 +9,16 @@ export enum EStages {
   SCIENCE = 'Science',
   GUILD = 'Guild',
   
+  //game base
   CITIES = 'Cities',
   LEADERS = 'Leaders',
   NAVAL = 'Naval',
   ISLAND = 'Island',
   
+  // architects
   MEDALS = 'Medals',
 
+  // duel
   AGORA = 'Agora',
   PANTHEON = 'Pantheon',
 }
