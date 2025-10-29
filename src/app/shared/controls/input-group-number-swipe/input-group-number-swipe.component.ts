1505fd61013dfd4a8f339b8f955aee08c8a1c81c
Carlos Astori <tadeu.astori@gmail.com>
2025-10-29 09:52:45 -0400

    refactory input group number component

--- src/app/shared/controls/input-group-number-swipe/input-group-number-swipe.component.ts
+++ src/app/shared/controls/input-group-number-swipe/input-group-number-swipe.component.ts
@@ -1,48 +1,50 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import 'hammerjs';
-import { HammerDirective } from 'src/app/core/directives/hammer.directive';
+import { InputNumberSwipeComponent } from "../input-number-swipe/input-number-swipe.component";
 
 @Component({
     selector: 'app-input-group-number-swipe',
-    imports: [HammerDirective, FormsModule], 
+    imports: [FormsModule, InputNumberSwipeComponent], 
     templateUrl: './input-group-number-swipe.component.html',
     styleUrl: './input-group-number-swipe.component.less'
 })
 export class InputGroupNumberSwipeComponent implements OnInit {
   @Input() values: number[] = [0, 0, 0, 0];
-  @Input() allowNegative: boolean = true;
-  sum: number = 0;
+  @Input() score: number = 0;
+  @Input() allowNegative: boolean = false;
+  @Input() allowPositive: boolean = true;
+  @Input() maximumPositiveNumber: number = 0;
+  @Input() minimumNegativeNumber: number = 0;  
+  @Input() displaySum: boolean = false;
+  @Output() valuesChanged = new EventEmitter<number[]>();
+
   isEditing: boolean = false;
   private labelTimeout: any;
 
-  ngOnInit() {
-    this.updateSum();
-  }
+  ngOnInit() {}
 
   hideLabel() {
     this.isEditing = true;
-    this.updateSum();
   }
 
-  onSwipeLeft(index: number) {
-    if (index < 3) {
-      this.hideLabel();
-      this.values[index]++;
-      this.updateSum();
-      this.updateD();
-      this.resetLabelTimeout();
+  resetLabelTimeout() {
+    if (this.labelTimeout) {
+      clearTimeout(this.labelTimeout);
     }
+
+    this.labelTimeout = setTimeout(() => {
+      this.isEditing = false;
+    }, 1000);
   }
 
-  onSwipeRight(index: number) {
-    if (index < 3 && this.allowNegative || this.values[index] > 0) {
-      this.hideLabel();
-      this.values[index]--;
-      this.updateSum();
-      this.updateD();
-      this.resetLabelTimeout();
-    }
+  onChangeValues(index: number, newValue: number) {
+    this.hideLabel();
+    this.values[index] = newValue;
+    this.updateD();
+    this.resetLabelTimeout();
+    
+    this.valuesChanged.emit(this.values);
   }
 
   onInputChange(event: any, index: number) {
@@ -55,41 +57,24 @@ export class InputGroupNumberSwipeComponent implements OnInit {
       event.target.value = this.values[index];
     }
 
-    this.updateSum();
     this.updateD();
   }
 
-  onInputFocus(event: any) {
+  onTouchStart(event: any) {
     this.isEditing = true;
     event.target.select();
-    this.updateSum();
   }
 
   onInputBlur() {
     this.resetLabelTimeout();
   }
 
-  updateSum() {
-    this.sum = this.values.slice(0, 3).reduce((a, b) => a + b, 0);
-  }
-
-  resetLabelTimeout() {
-    if (this.labelTimeout) {
-      clearTimeout(this.labelTimeout);
-    }
-
-    this.labelTimeout = setTimeout(() => {
-      this.isEditing = false;
-      this.updateSum();
-    }, 1000);
-  }
-
   updateD() {  
-  const firstThree = this.values.slice(0, 3).filter(n => n !== null && n >= 0);
-  if (firstThree.length > 0) {
-    this.values[3] = Math.min(...firstThree);
-  } else {
-    this.values[3] = 0;
+    const firstThree = this.values.slice(0, 3).filter(n => n !== null && n >= 0);
+    if (firstThree.length > 0) {
+      this.values[3] = Math.min(...firstThree);
+    } else {
+      this.values[3] = 0;
+    }
   }
-}
 }
