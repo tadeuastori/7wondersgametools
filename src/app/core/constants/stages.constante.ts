61fd648bbd6a2c9cdf393f530313d35da5093729
Carlos Astori <tadeu.astori@gmail.com>
2025-10-29 09:55:08 -0400

    added positive and negative properties

--- src/app/core/constants/stages.constante.ts
+++ src/app/core/constants/stages.constante.ts
@@ -5,86 +5,114 @@ export const stagesList: IStage[] = [{
     stage: EStages.WONDER,
     order: 0,
     color: '#E7E8E9',
-    icon: ''
+    icon: '',
+    hasNegativeValue: false,
+    hasPositiveValue: true
 },
 {
     stage: EStages.COIN,
     order: 1,
     color: '#FEFCD7',
-    icon: ''
+    icon: '',
+    hasNegativeValue: false,
+    hasPositiveValue: true
 },
 {
     stage: EStages.MILITARY,
     order: 3,    
     color: '#F3CFC1',
-    icon: ''
+    icon: '',
+    hasNegativeValue: true,
+    hasPositiveValue: true
 },
 {
     stage: EStages.CIVIC,
     order: 4,
     color: '#C8EBFB',
-    icon: ''
+    icon: '',
+    hasNegativeValue: false,
+    hasPositiveValue: true
 },
 {
     stage: EStages.COMMERCE,
     order: 5,
     color: '#FEFCD7',
-    icon: ''
+    icon: '',
+    hasNegativeValue: false,
+    hasPositiveValue: true
 },
 {
     stage: EStages.SCIENCE,
     order: 6,
     color: '#D9E9D2',
-    icon: ''
+    icon: '',
+    hasNegativeValue: false,
+    hasPositiveValue: true
 },
 {
     stage: EStages.GUILD,
     order: 7,
     color: '#D1D4EA',
-    icon: ''
+    icon: '',
+    hasNegativeValue: false,
+    hasPositiveValue: true
 },
 //Game base
 {
     stage: EStages.CITIES,
     order: 8,
     color: '#A6A7AB',
-    icon: ''
+    icon: '',
+    hasNegativeValue: true,
+    hasPositiveValue: true
 },
 {
     stage: EStages.DEBT,
     order: 2,
     color: '#FEFEFE',
-    icon: ''
+    icon: '',
+    hasNegativeValue: true,
+    hasPositiveValue: true
 },
 {
     stage: EStages.LEADERS,
     order: 9,
     color: '#D1D2D4',
-    icon: ''
+    icon: '',
+    hasNegativeValue: true,
+    hasPositiveValue: true
 },
 {
     stage: EStages.NAVAL,
     order: 10,
     color: '#9FCDED',
-    icon: ''
+    icon: '',
+    hasNegativeValue: true,
+    hasPositiveValue: true
 },
 {
     stage: EStages.ISLAND,
     order: 11,
     color: '#CCE3F5',
-    icon: ''
+    icon: '',
+    hasNegativeValue: true,
+    hasPositiveValue: true
 },
 //Architects
 {
     stage: EStages.MEDALS,
     order: 12,
     color: '#F9E8C7',
-    icon: ''
+    icon: '',
+    hasNegativeValue: true,
+    hasPositiveValue: true
 },
 //Duel
 {
     stage: EStages.PANTHEON,
     order: 13 ,
     color: '#FEFEFE',
-    icon: ''
+    icon: '',
+    hasNegativeValue: true,
+    hasPositiveValue: true
 }];
--- src/app/core/models/game/stage.model.ts
+++ src/app/core/models/game/stage.model.ts
@@ -5,6 +5,8 @@ export interface IStage {
     order: number;
     color: string;
     icon: string;
+    hasNegativeValue: boolean;
+    hasPositiveValue: boolean
 }
 
 export class Stage implements IStage {
@@ -12,17 +14,24 @@ export class Stage implements IStage {
     order: number
     color: string
     icon: string
+    hasNegativeValue: boolean
+    hasPositiveValue: boolean
 
     constructor(clone?: IStage) {
         this.stage = EStages.WONDER;
         this.order = 0;
         this.color = '#000000';
         this.icon = '';
+        this.hasNegativeValue = false;
+        this.hasPositiveValue = true;
+
         if (clone) {
             this.stage = clone.stage;
             this.order = clone.order;
             this.color = clone.color;
             this.icon = clone.icon;
+            this.hasNegativeValue = clone.hasNegativeValue;
+            this.hasPositiveValue = clone.hasPositiveValue;
         }
     }
 }
