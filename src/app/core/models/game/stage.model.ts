5173768ae4c7e52f4b85ae78c9f67822388e47d1
Carlos Astori <tadeu.astori@gmail.com>
2025-10-31 15:09:11 -0400

    updated models

--- src/app/core/models/game/stage.model.ts
+++ src/app/core/models/game/stage.model.ts
@@ -3,27 +3,30 @@ import { EStages } from "../../enums/stages.enum";
 export interface IStage {
     stage: EStages;
     order: number;
-    color: string;
+    color: string[]; //[BGColor, TXTColor]
     icon: string;
     hasNegativeValue: boolean;
-    hasPositiveValue: boolean
+    hasPositiveValue: boolean;
+    hasScoreCalculated: boolean;
 }
 
 export class Stage implements IStage {
-    stage: EStages
-    order: number
-    color: string
-    icon: string
-    hasNegativeValue: boolean
-    hasPositiveValue: boolean
+    stage: EStages;
+    order: number;
+    color: string[];
+    icon: string;
+    hasNegativeValue: boolean;
+    hasPositiveValue: boolean;
+    hasScoreCalculated: boolean = false;
 
     constructor(clone?: IStage) {
         this.stage = EStages.WONDER;
         this.order = 0;
-        this.color = '#000000';
+        this.color = ['#000000','#000000']; 
         this.icon = '';
         this.hasNegativeValue = false;
         this.hasPositiveValue = true;
+        this.hasScoreCalculated = false;
 
         if (clone) {
             this.stage = clone.stage;
@@ -32,6 +35,7 @@ export class Stage implements IStage {
             this.icon = clone.icon;
             this.hasNegativeValue = clone.hasNegativeValue;
             this.hasPositiveValue = clone.hasPositiveValue;
+            this.hasScoreCalculated = clone.hasScoreCalculated;
         }
     }
 }
